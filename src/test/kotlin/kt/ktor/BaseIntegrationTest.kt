d1f8423a9eb1b812dd9c5865204be7265424d715
sammetav <vishnu.sammeta@tset.com>
2025-07-19 18:13:22 +0200

    greens

--- src/test/kotlin/kt/ktor/BaseIntegrationTest.kt
+++ src/test/kotlin/kt/ktor/BaseIntegrationTest.kt
@@ -1,5 +1,8 @@
 package kt.ktor
 
+import org.flywaydb.core.Flyway
+import org.flywaydb.core.api.Location
+import org.flywaydb.core.api.configuration.FluentConfiguration
 import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
@@ -15,9 +18,7 @@ import org.testcontainers.utility.DockerImageName
 abstract class BaseIntegrationTest {
 
     companion object {
-
         private val logger = LoggerFactory.getLogger(BaseIntegrationTest::class.java)
-
         private const val VERSION = "postgres:14.6"
         private const val DB_NAME = "data"
         private const val DB_USER = "postgres"
@@ -27,6 +28,7 @@ abstract class BaseIntegrationTest {
             .withDatabaseName(DB_NAME)
             .withUsername(DB_USER)
             .withPassword(DB_PWD)
+            .apply { start() }
 
         @JvmStatic
         @DynamicPropertySource
@@ -34,7 +36,6 @@ abstract class BaseIntegrationTest {
             registry.add("spring.r2dbc.url", Companion::r2dbcUrl)
             registry.add("spring.r2dbc.username", db::getUsername)
             registry.add("spring.r2dbc.password", db::getPassword)
-
             registry.add("postgres.host", db::getHost)
             registry.add("postgres.port") { db.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT) }
             registry.add("postgres.database", db::getDatabaseName)
@@ -44,5 +45,16 @@ abstract class BaseIntegrationTest {
 
         private fun r2dbcUrl(): String =
             "r2dbc:postgresql://${db.host}:${db.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT)}/${db.databaseName}"
+
+        init {
+            logger.info("Starting postgres test container")
+            // Run Flyway migrations before tests
+            val flyway = Flyway.configure()
+                .locations(Location("classpath:db/migration"))
+                .dataSource(db.jdbcUrl, DB_USER, DB_PWD)
+                .load()
+            logger.info("Executing flyway migrations for test DB")
+            flyway.migrate()
+        }
     }
 }
